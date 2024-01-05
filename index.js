8159176888c5e4d873ad2e2e167de6961dcbf08a
Konok Mehorab Shibli <mehorabkonok@gmail.com>
2024-01-05 17:44:53 +0600

    Clear button and Reset button has been added along with their functionality.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 let divEl = document.getElementById('content');
 let buttonEl = document.getElementById('change-button');
-
+let clearEl = document.getElementById('clear-button');
+let mainEl = document.getElementById('main');
+let resetEl = document.getElementById('reset-button');
 
 buttonEl.addEventListener('click', function () {
     console.log('Button has toggeled');
-    divEl.classList.toggle('red');
+    divEl.classList.toggle('blue');
+
+    mainEl.classList.toggle('night-theme');
+})
+
+clearEl.addEventListener('click', function () {
+    console.log('Cleared!');
+    divEl.innerText = '';
+})
+
+resetEl.addEventListener('click', function () {
+    console.log('Reset text.');
+    divEl.innerHTML = 'Hello . This is programming hero GitHub Testing';
 })
